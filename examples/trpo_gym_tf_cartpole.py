82fe819dd90a74b3e53b9ff79402cc3c1479e82a
dbueno96 <daniel.bueno@correounivalle.edu.co>
2019-07-02 20:39:44 -0500

    all

---
+++ examples/trpo_gym_tf_cartpole.py
@@ -0,0 +1,44 @@
+from rllab.baselines.linear_feature_baseline import LinearFeatureBaseline
+from rllab.envs.gym_env import GymEnv
+from rllab.envs.normalized_env import normalize
+from rllab.misc.instrument import stub, run_experiment_lite
+print('Importing Sandbox')
+from sandbox.rocky.tf.envs.base import TfEnv
+print('1')
+from sandbox.nrocky.tf.policies.categorical_mlp_policy import CategoricalMLPPolicy
+print ('2')
+from sandbox.rocky.tf.algos.trpo import TRPO
+print('3')
+stub(globals())
+
+# Need to wrap in a tf environment and force_reset to true
+# see https://github.com/openai/rllab/issues/87#issuecomment-282519288
+env = TfEnv(normalize(GymEnv("CartPole-v0", force_reset=True)))
+
+policy = CategoricalMLPPolicy(
+name="policy",
+env_spec=env.spec,
+# The neural network policy should have two hidden layers, each with 32 hidden units.
+hidden_sizes=(32, 32)
+)
+
+baseline = LinearFeatureBaseline(env_spec=env.spec)
+
+algo = TRPO(
+    env=env,
+    policy=policy,
+    baseline=baseline,
+    batch_size=4000,
+    max_path_length=200,
+    n_itr=120,
+    discount=0.99,
+    step_size=0.01,
+    # optimizer=ConjugateGradientOptimizer(hvp_approach=FiniteDifferenceHvp(base_eps=1e-5))
+)
+
+run_experiment_lite(
+    algo.train(),
+    n_parallel=1,
+    snapshot_mode="last",
+    seed=1
+)
